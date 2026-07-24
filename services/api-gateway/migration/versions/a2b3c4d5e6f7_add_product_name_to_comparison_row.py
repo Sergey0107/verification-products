7e9ad8ddd52ce5fd65594b9c05288990a55f8836
sergey <s.tatarskikh@modimio.ru>
2026-07-24 10:56:23 +0300

    Исправить коллизию revision ID в новой миграции
    a1b2c3d4e5f6 уже использовался существующей миграцией
    (add_completed_at_and_fix_user_result) — alembic upgrade падал с
    "Cycle is detected in revisions". Новая миграция переименована в
    a2b3c4d5e6f7 с тем же содержимым.

--- services/api-gateway/migration/versions/a2b3c4d5e6f7_add_product_name_to_comparison_row.py
+++ services/api-gateway/migration/versions/a2b3c4d5e6f7_add_product_name_to_comparison_row.py
@@ -1,6 +1,6 @@
 """add product_name to comparison_row
 
-Revision ID: a1b2c3d4e5f6
+Revision ID: a2b3c4d5e6f7
 Revises: f8a9b0c1d2e3
 Create Date: 2026-07-24 12:00:00.000000
 
@@ -10,7 +10,7 @@ from typing import Sequence, Union
 from alembic import op
 import sqlalchemy as sa
 
-revision: str = "a1b2c3d4e5f6"
+revision: str = "a2b3c4d5e6f7"
 down_revision: Union[str, Sequence[str], None] = "f8a9b0c1d2e3"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
